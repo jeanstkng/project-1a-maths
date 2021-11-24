f41f3366a2a9e287d4c331d22404d464c588f8e1
Jean Castillo <jean.castillo@devsu.com>
2021-11-24 16:41:03 -0500

    first version of maths module

---
+++ index.js
@@ -0,0 +1,46 @@
+module.exports = {
+  /**
+   * Sum of two numbers
+   * @example
+   * n1 = 1, n2 = 3 = > result 4
+   * @param {*} n1 First element of the sum
+   * @param {*} n2 Second element of the sum
+   * @returns
+   */
+  sum: function (n1, n2) {
+    return n1 + n2;
+  },
+  /**
+   * Substract of two numbers
+   * @example
+   * n1 = 1, n2 = 3 = > result -2
+   * @param {*} n1 First element of the substract
+   * @param {*} n2 Second element of the substract
+   * @returns
+   */
+  substract: function (n1, n2) {
+    return n1 - n2;
+  },
+  /**
+   * Multiplication of two numbers
+   * @example
+   * n1 = 1, n2 = 3 = > result 3
+   * @param {*} n1 First element of the multiplication
+   * @param {*} n2 Second element of the multiplication
+   * @returns
+   */
+  multiply: function (n1, n2) {
+    return n1 * n2;
+  },
+  /**
+   * Division of two numbers
+   * @example
+   * n1 = 1, n2 = 3 = > result 0.33
+   * @param {*} n1 First element of the division
+   * @param {*} n2 Second element of the division
+   * @returns
+   */
+  divide: function (n1, n2) {
+    return n1 / n2;
+  },
+};
---
+++ pruebas.js
@@ -0,0 +1,13 @@
+const mathModule = require('.');
+
+console.log(mathModule.sum(1, 2));
+
+console.log(mathModule.sum(1, 2));
+
+console.log(mathModule.substract(11, 24));
+
+console.log(mathModule.substract(1, 2));
+
+console.log(mathModule.multiply(11, 24));
+
+console.log(mathModule.multiply(1, 2));
